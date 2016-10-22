2de5b973cdfd27596cccae59e04942ef1e71f385
pradyumnat <pradyumna@servelots.com>
2016-10-22 09:26:20 +0530

    first commit

---
+++ data2.js
@@ -0,0 +1,134 @@
+var data = [
+ {
+   "Name": "Obaid Siddiqi",
+   "Affiliation": "TIFR, TIFR Centre, NCBS",
+   "Relevance": "Founding member, TIFR Molecular Biology Unit and NCBS",
+   "AudioUrl": "https://www.ncbs.res.in/ncbs25dev/ncbs_files/theme_8/Peopleplex_Audio/OS_IARI-hailstorm_RESEARCH-INFLECTION.mp3",
+   "AudioCaption": "Obaid Siddiqi, founding member of NCBS & TIFR's molecular biology unit: Memories of a hail storm that destroyed his research at the Indian Agricultural Research Institute in the 1950s, prompting a move to Glasgow for his PhD",
+   "AudioRights": "TIFR Archives",
+   "ImageUrl": "https://www.ncbs.res.in/ncbs25dev/ncbs_files/theme_8/Peopleplex_Image/Profile%20-%20OS.JPG",
+   "ImageCaption": "Obaid Siddiqi. 2012",
+   "ImageRights": ""
+ },
+ {
+   "Name": "M S Swaminathan",
+   "Affiliation": "Indian Agricultural Research Institute",
+   "Relevance": "Suggests a National Biological Laboratory in the early 1960s; Advises O. Siddiqi to go abroad to study after Siddiqi's wheat crop got destroyed in a hailstorm; In Planning Commission that reviews and approves NCBS proposal in 1980-85",
+   "AudioUrl": "https://www.ncbs.res.in/ncbs25dev/ncbs_files/theme_8/Peopleplex_Audio/swaminathan_NBL_IDENTITY-SPACE.mp3",
+   "AudioCaption": "MS Swaminathan, member of Planning Commission in 1980: Reflecting on a 1960s proposal for a National Biological Laboratory, and seeing the TIFR-IISc joint biology centre proposal while in the Planning Commission in 1980.",
+   "AudioRights": "NCBS Archives",
+   "ImageUrl": "https://www.ncbs.res.in/ncbs25dev/ncbs_files/theme_8/Peopleplex_Image/Profile%20-%20MS%20Swaminathan.JPG",
+   "ImageCaption": "MS Swaminathan",
+   "ImageRights": ""
+ },
+ {
+   "Name": "K VijayRaghavan",
+   "Affiliation": "TIFR, TIFR Centre, NCBS",
+   "Relevance": "Cancels plans to go to Cornell after 1975 Emergency. Stays at IIT Kanpur for a masters. Then, defers plans to go for a PhD at ETH Zurich in 1977 after a tuberculosis bout. Captivated by an Obaid Siddiqi paper on neurogenetics while doing a temporary fellowship at AIIMS. Applies to TIFR for a PhD. Faculty and founding member, NCBS.",
+   "AudioUrl": "https://www.ncbs.res.in/ncbs25dev/ncbs_files/theme_8/Peopleplex_Audio/vijay_AIIMS-TB-OS-paper-1975_PEOPLE-ORIGIN.mp3",
+   "AudioCaption": "K VijayRaghavan, faculty member at NCBS: The path to TIFR after a chance reading of an Obaid Siddiqi paper while recuperating from tuberculosis in Delhi",
+   "AudioRights": "NCBS Archives",
+   "ImageUrl": "https://www.ncbs.res.in/ncbs25dev/ncbs_files/theme_8/Peopleplex_Image/Profile%20-%20Vijay.JPG",
+   "ImageCaption": "K VijayRaghavan. 2012",
+   "ImageRights": ""
+ },
+ {
+   "Name": "Sanjay Sane",
+   "Affiliation": "TIFR, NCBS",
+   "Relevance": "Joins TIFR/NCBS as one of the first few JRFs in the early 1990s after listening to a talk by K VijayRaghavan. Joins NCBS as faculty in 2007.",
+   "AudioUrl": "https://www.ncbs.res.in/ncbs25dev/ncbs_files/theme_8/Peopleplex_Audio/sanjay_tifr-veronica_EDUCATION-MENTOR.mp3",
+   "AudioCaption": "Sanjay Sane, faculty member and former student at NCBS: Entering TIFR as a student and early interactions with Veronica Rodrigues.",
+   "AudioRights": "NCBS Archives",
+   "ImageUrl": "https://www.ncbs.res.in/ncbs25dev/ncbs_files/theme_8/Peopleplex_Image/Profile%20-%20Sanjay%20Sane.jpg",
+   "ImageCaption": "Sanjay Sane",
+   "ImageRights": ""
+ },
+ {
+   "Name": "Vidyanand Nanjundiah",
+   "Affiliation": "TIFR",
+   "Relevance": "1980s faculty at TIFR MBU who works on developmental biology and a new model system for the group, slime mold. Also shapes the NCBS 1985-1990 proposal with O.Siddiqi. Helped NCBS initially in locating land in Bangalore",
+   "AudioUrl": "https://www.ncbs.res.in/ncbs25dev/ncbs_files/theme_8/Peopleplex_Audio/nanjundiah_1976-80-IISc-TIFR_IDENTITY-SPACE.mp3",
+   "AudioCaption": "Vidyanand Nanjundiah, faculty member at TIFR in 1980s: On his move from IISc to TIFR, and reflections on the newly proposed (c 1981) IISc-TIFR joint centre for biology.",
+   "AudioRights": "NCBS Archives",
+   "ImageUrl": "https://www.ncbs.res.in/ncbs25dev/ncbs_files/theme_8/Peopleplex_Image/Profile%20-%20Nanjundiah.JPG",
+   "ImageCaption": "Vidyanand Nanjundiah. 2012",
+   "ImageRights": ""
+ },
+ {
+   "Name": "T M Sahadevan",
+   "Affiliation": "TIFR, TIFR Centre, NCBS",
+   "Relevance": "Joins TIFR in the early 1970s, and navigates the administration for the mathematics, radio astronomy and finally, biological sciences groups' activities in Bangalore. Becomes an indispensable glue, especially in the early days of NCBS. ",
+   "AudioUrl": "https://www.ncbs.res.in/ncbs25dev/ncbs_files/theme_8/Peopleplex_Audio/sahadevan_TIFR-1972_PEOPLE-ORIGIN.mp3",
+   "AudioCaption": "TM Sahadevan, long-time administrative architect at NCBS: An early career move from Coonoor to Bangalore, and being the first person from TIFR, Bombay, to land at the TIFR Centre, IISc in the mid 1970s.",
+   "AudioRights": "NCBS Archives",
+   "ImageUrl": "https://www.ncbs.res.in/ncbs25dev/ncbs_files/theme_8/Peopleplex_Image/Profile%20-%20TM%20Sahadevan.jpg",
+   "ImageCaption": "TM Sahadevan. 1998",
+   "ImageRights": ""
+ },
+ {
+   "Name": "Mathew K Mathew",
+   "Affiliation": "TIFR, TIFR Centre, NCBS",
+   "Relevance": "Founding faculty member of NCBS. Recruited after his post-doctoral work at Caltech/UCSF, where he meets Mani Ramaswami and K VijayRaghavan. Instrumental in implementing an academic curriculum on campus. Worked with the constructon team when GKVK campus was being built.",
+   "AudioUrl": "https://www.ncbs.res.in/ncbs25dev/ncbs_files/theme_8/Peopleplex_Audio/Mathew_IIT-IISc_PEOPLE-ORIGIN.mp3",
+   "AudioCaption": "MK Mathew, faculty member at NCBS: Reflections on early education and figuring out what direction to take his life in.",
+   "AudioRights": "NCBS Archives",
+   "ImageUrl": "https://www.ncbs.res.in/ncbs25dev/ncbs_files/theme_8/Peopleplex_Image/Profile%20-%20MK%20Mathew.jpg",
+   "ImageCaption": "MK Mathew ",
+   "ImageRights": ""
+ },
+ {
+   "Name": "Mitradas Panicker",
+   "Affiliation": "TIFR, TIFR Centre, NCBS",
+   "Relevance": "One of the first wave of faculty hires; joins in 1992. Spent a summer at TIFR during Masters. Recruited after his post-doctoral work at UC Irvine and an interview with O.Siddiqi at Caltech. Worked with the construction team when the GKVK campus was being built.",
+   "AudioUrl": "https://www.ncbs.res.in/ncbs25dev/ncbs_files/theme_8/Peopleplex_Audio/panic_TIFR-summer_PEOPLE-ORIGIN.mp3",
+   "AudioCaption": "Mitradas Panicker, faculty member at NCBS: An early introduction to TIFR as a summer student in PK Maitra's lab",
+   "AudioRights": "NCBS Archives",
+   "ImageUrl": "https://www.ncbs.res.in/ncbs25dev/ncbs_files/theme_8/Peopleplex_Image/Profile%20-%20Mitradas%20Panicker.jpg",
+   "ImageCaption": "Mitradas Panicker",
+   "ImageRights": ""
+ },
+ {
+   "Name": "N. Shanthakumary",
+   "Affiliation": "TIFR Centre, NCBS",
+   "Relevance": "Joined TIFR Centre for work as a stenographer in 1983 and moved to NCBS after its formation. Early staff who got involved in all tasks, including accounting, office administration, scientific typing, revision of NCBS project cost and preparation of proposals. ",
+   "AudioUrl": "https://www.ncbs.res.in/ncbs25dev/ncbs_files/theme_8/Peopleplex_Audio/shantha_hiring-walk-IISc_PEOPLE-ORIGIN.mp3",
+   "AudioCaption": "N Shanthakumary, early hire in NCBS administration: Memories of entering the IISc campus for the first time in the early 1980s, and being hired into the system.",
+   "AudioRights": "NCBS Archives",
+   "ImageUrl": "https://www.ncbs.res.in/ncbs25dev/ncbs_files/theme_8/Peopleplex_Image/Profile%20-%20N%20Shanthakumary.jpg",
+   "ImageCaption": "N Shanthakumary (left). 1999",
+   "ImageRights": ""
+ },
+ {
+   "Name": "U B Poornima",
+   "Affiliation": "TIFR Centre, NCBS",
+   "Relevance": "Hired as an architect for NCBS, first to realize the design of Raj Rewal's team, and then, to implement harmonious expansions of the campus.",
+   "AudioUrl": "https://www.ncbs.res.in/ncbs25dev/ncbs_files/theme_8/Peopleplex_Audio/poornima_emergency-home-sci_PEOPLE-ORIGIN.mp3",
+   "AudioCaption": "UB Poornima, chief architect at NCBS: Early career circumstances and choosing architecture as a career.",
+   "AudioRights": "NCBS Archives",
+   "ImageUrl": "https://www.ncbs.res.in/ncbs25dev/ncbs_files/theme_8/Peopleplex_Image/Profile%20-%20Poornima.jpg",
+   "ImageCaption": "UB Poornima (right). 1995",
+   "ImageRights": ""
+ },
+ {
+   "Name": "Shaju Varghese",
+   "Affiliation": "TIFR, TIFR Centre, NCBS",
+   "Relevance": "Started as canteen supervisor at TIFR in the early 1980s. Hired to NCBS at TIFR Centre in the early 1990s. Starts canteen and kitchen with one stove. Currently responsible for hospitality, canteen, hostel, guest house and security in both main and Yelahanka campuses of NCBS.",
+   "AudioUrl": "https://www.ncbs.res.in/ncbs25dev/ncbs_files/theme_8/Peopleplex_Audio/shaju_childhood-labor-catering_PEOPLE-ORIGIN.mp3",
+   "AudioCaption": "Shaju Varghese, hospitality/security/canteen supervisor: Reflections on a childhood in Kerala, and the connections with his work today.",
+   "AudioRights": "NCBS Archives",
+   "ImageUrl": "https://www.ncbs.res.in/ncbs25dev/ncbs_files/theme_8/Peopleplex_Image/Profile%20-%20Shaju%20Varghese.jpg",
+   "ImageCaption": "Shaju Varghese (second from left). 2002",
+   "ImageRights": "NCBS Archives"
+ },
+ {
+   "Name": "P P Ranjith",
+   "Affiliation": "TIFR Centre, NCBS",
+   "Relevance": "Joins NCBS in a temporary position for 6 months in 1995. Later hired as a \"JRF\" in Satyajit Mayor's group before a permanent position as lab manager. Had a major role in shifting all laboratories from TIFR Centre to the new campus. Often hailed as an indispensable glue to the functioning of the campus.",
+   "AudioUrl": "https://www.ncbs.res.in/ncbs25dev/ncbs_files/theme_8/Peopleplex_Audio/ranjith_kerala-maths_PEOPLE-ORIGIN.mp3",
+   "AudioCaption": "PP Ranjith, early hire as lab manager at NCBS: A degree in mathematics, and the circumstances behind a move from Kerala in search of a job.",
+   "AudioRights": "NCBS Archives",
+   "ImageUrl": "https://www.ncbs.res.in/ncbs25dev/ncbs_files/theme_8/Peopleplex_Image/Profile%20-%20PP%20Ranjith.jpg",
+   "ImageCaption": "P Ranjith. 1998",
+   "ImageRights": ""
+ }
+]
